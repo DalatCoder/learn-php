b700283d50e7734c4babbd269459b0d6d19d3cb0
Trong Hieu <hieuntctk42@gmail.com>
2021-01-11 10:44:50 +0700

    Connect to mysql server

--- 07.php-mysql/form_process.php
+++ 07.php-mysql/form_process.php
@@ -1,13 +1,31 @@
 <?php
-    print_r($_POST);
-
     if (isset($_POST['submit'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
         echo '<br>';
+        if (!($username && $password)) {
+            echo 'Please enter your credential';
+            return;
+        }
+
         echo 'Your username is: ' . $username;
         echo '<br>';
         echo 'Your password is: ' . $password;
+
+        $db_server = 'localhost';
+        $db_username = 'root';
+        $db_password = '';
+        $db_name = 'loginapp';
+
+        $connection = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+
+        echo '<br>';
+        if (!$connection) {
+            die("Oops something went wrong :(");
+            return;
+        }
+
+        echo 'Connect to database successfully!';
     }
 ?>

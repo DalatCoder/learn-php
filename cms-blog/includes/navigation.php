8534f5e4d4ef016df53e44a820ba8375ccb5875c
Trong Hieu <hieuntctk42@gmail.com>
2021-01-12 14:19:16 +0700

    Admin page 🎂🎂🎂

--- cms-blog/includes/navigation.php
+++ cms-blog/includes/navigation.php
@@ -30,6 +30,9 @@
                         ";
                     }
                 ?>
+                <li>
+                    <a href="admin">Admin</a>
+                </li>
             </ul>
         </div>
         <!-- /.navbar-collapse -->

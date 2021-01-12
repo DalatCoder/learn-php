4f49f66370cb230ce558f1730b3127764c0ab59e
Trong Hieu <hieuntctk42@gmail.com>
2021-01-12 11:11:37 +0700

    Fetch and display list of posts 🎂🎂🎂

--- cms-blog/index.php
+++ cms-blog/index.php
@@ -25,8 +25,41 @@
                     <small>Secondary Text</small>
                 </h1>
 
-                <!-- First Blog Post -->
-                <?php include "includes/blog.php"; ?>
+                <?php
+                    $query = "SELECT * FROM Posts";
+                    $select_all_posts_query = mysqli_query($connection, $query);
+                    if (!$select_all_posts_query) {
+                        die('Oops! Error when fetching list of posts');
+                        return;
+                    }
+
+                    while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_image = $row['post_image'];
+                        $post_content = $row['post_content'];
+                        ?>
+                        <h2>
+                            <a href="#"><?php echo $post_title; ?></a>
+                        </h2>
+                        <p class="lead">
+                            by <a href="index.php"><?php echo $post_author; ?></a>
+                        </p>
+                        <p>
+                            <span class="glyphicon glyphicon-time"></span>
+                            Posted on <?php echo $post_date; ?>
+                        </p>
+                        <hr>
+                        <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                        <hr>
+                        <p><?php echo $post_content; ?></p>
+                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                        <hr>
+                        <?php
+                    }
+                ?>
 
                 <!-- Pager -->
                 <ul class="pager">

e9defd2712bfec59b9c1f537ddb98c93780cb947
Trong Hieu <hieuntctk42@gmail.com>
2021-01-13 14:33:06 +0700

    Display individual post

--- cms-blog/index.php
+++ cms-blog/index.php
@@ -9,91 +9,93 @@
 
 <body>
 
-    <!-- Navigation -->
-    <?php include "includes/navigation.php" ?>;
+<!-- Navigation -->
+<?php include "includes/navigation.php" ?>;
 
-    <!-- Page Content -->
-    <div class="container">
+<!-- Page Content -->
+<div class="container">
 
-        <div class="row">
+    <div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+            <h1 class="page-header">
+                Page Heading
+                <small>Secondary Text</small>
+            </h1>
 
-                <?php
-                    $query = "SELECT * FROM Posts";
-                    $select_all_posts_query = mysqli_query($connection, $query);
-                    if (!$select_all_posts_query) {
-                        die('Oops! Error when fetching list of posts');
-                        return;
-                    }
+            <?php
+            $query = "SELECT * FROM Posts";
+            $select_all_posts_query = mysqli_query($connection, $query);
+            if (!$select_all_posts_query) {
+                die('Oops! Error when fetching list of posts');
+                return;
+            }
 
-                    while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                        $post_title = $row['post_title'];
-                        $post_author = $row['post_author'];
-                        $post_date = $row['post_date'];
-                        $post_image = $row['post_image'];
-                        $post_content = $row['post_content'];
-                        ?>
-                        <h2>
-                            <a href="#"><?php echo $post_title; ?></a>
-                        </h2>
-                        <p class="lead">
-                            by <a href="index.php"><?php echo $post_author; ?></a>
-                        </p>
-                        <p>
-                            <span class="glyphicon glyphicon-time"></span>
-                            Posted on <?php echo $post_date; ?>
-                        </p>
-                        <hr>
-                        <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                        <hr>
-                        <p><?php echo $post_content; ?></p>
-                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                        <hr>
-                        <?php
-                    }
+            while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                $post_id = $row['post_id'];
+                $post_title = $row['post_title'];
+                $post_author = $row['post_author'];
+                $post_date = $row['post_date'];
+                $post_image = $row['post_image'];
+                $post_content = $row['post_content'];
                 ?>
+                <h2>
+                    <a href="post.php?post_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="#"><?php echo $post_author; ?></a>
+                </p>
+                <p>
+                    <span class="glyphicon glyphicon-time"></span>
+                    Posted on <?php echo $post_date; ?>
+                </p>
+                <hr>
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                <hr>
+                <p><?php echo $post_content; ?></p>
+                <a class="btn btn-primary" href="post.php?post_id=<?php echo $post_id; ?>">Read More <span
+                            class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <!-- Pager -->
-                <ul class="pager">
-                    <li class="previous">
-                        <a href="#">&larr; Older</a>
-                    </li>
-                    <li class="next">
-                        <a href="#">Newer &rarr;</a>
-                    </li>
-                </ul>
+                <hr>
+                <?php
+            }
+            ?>
 
-            </div>
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-                <?php include "includes/sidebar.php"; ?>
-            </div>
+            <!-- Pager -->
+            <ul class="pager">
+                <li class="previous">
+                    <a href="#">&larr; Older</a>
+                </li>
+                <li class="next">
+                    <a href="#">Newer &rarr;</a>
+                </li>
+            </ul>
 
         </div>
-        <!-- /.row -->
 
-        <hr>
-
-        <!-- Footer -->
-        <?php include "includes/footer.php"; ?>
+        <!-- Blog Sidebar Widgets Column -->
+        <div class="col-md-4">
+            <?php include "includes/sidebar.php"; ?>
+        </div>
 
     </div>
-    <!-- /.container -->
+    <!-- /.row -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <hr>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <!-- Footer -->
+    <?php include "includes/footer.php"; ?>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
 
 </body>
 

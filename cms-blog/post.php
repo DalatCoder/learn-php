71f170f2987fa028c6f905ca8dd9666eb6893bf0
Trong Hieu <hieuntctk42@gmail.com>
2021-01-13 17:45:33 +0700

    Display comments base on approval

--- cms-blog/post.php
+++ cms-blog/post.php
@@ -92,11 +92,13 @@
                         <form role="form" action="" method="post">
                             <div class="form-group">
                                 <label for="author">Author</label>
-                                <input type="text" id="author" name="comment_author" class="form-control" placeholder="Author name">
+                                <input type="text" id="author" name="comment_author" class="form-control"
+                                       placeholder="Author name">
                             </div>
                             <div class="form-group">
                                 <label for="email">Email</label>
-                                <input type="email" id="email" name="comment_email" class="form-control" placeholder="Author email">
+                                <input type="email" id="email" name="comment_email" class="form-control"
+                                       placeholder="Author email">
                             </div>
                             <div class="form-group">
                                 <label for="content">Content</label>
@@ -111,57 +113,36 @@
                     <!-- Posted Comments -->
 
                     <!-- Comment -->
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
-                            commodo.
-                            Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
-                            ac
-                            nisi
-                            vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
-                    </div>
+                    <?php
+                    $post_id = $_GET['post_id'];
+                    $query = "SELECT * FROM Comments WHERE comment_post_id = $post_id ";
+                    $query .= "AND comment_status = 'approved' ";
+                    $query .= "ORDER BY comment_id DESC";
+                    
+                    $select_approved_comments_query = mysqli_query($connection, $query);
+                    if (!$select_approved_comments_query) {
+                        die('Oops! Error when fetching comments data ' . mysqli_error($connection));
+                    }
 
-                    <!-- Comment -->
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
-                            commodo.
-                            Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
-                            ac
-                            nisi
-                            vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            <!-- Nested Comment -->
-                            <div class="media">
-                                <a class="pull-left" href="#">
-                                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                                </a>
-                                <div class="media-body">
-                                    <h4 class="media-heading">Nested Start Bootstrap
-                                        <small>August 25, 2014 at 9:30 PM</small>
-                                    </h4>
-                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                                    sollicitudin
-                                    commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce
-                                    condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in
-                                    faucibus.
-                                </div>
+                    while ($row = mysqli_fetch_assoc($select_approved_comments_query)) {
+                        $comment_author = $row['comment_author'];
+                        $comment_date = $row['comment_date'];
+                        $comment_content = $row['comment_content'];
+                        ?>
+                        <div class="media">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" src="http://placehold.it/64x64" alt="">
+                            </a>
+                            <div class="media-body">
+                                <h4 class="media-heading"><?php echo $comment_author; ?>
+                                    <small><?php echo $comment_date; ?></small>
+                                </h4>
+                                <?php echo $comment_content; ?>
                             </div>
-                            <!-- End Nested Comment -->
                         </div>
-                    </div>
+                        <?php
+                    }
+                    ?>
                     <?php
                 }
             }

bb0c204767cf2041a84557db7d71c8ce4ab5a625
Trong Hieu <hieuntctk42@gmail.com>
2021-01-14 21:20:08 +0700

    Add markup for registration 🐶🐶🐶

---
+++ cms-blog/registration.php
@@ -0,0 +1,67 @@
+<?php include "includes/db.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "includes/head.php"; ?>
+</head>
+
+<body>
+
+<!-- Navigation -->
+<?php include "includes/navigation.php" ?>;
+
+<!-- Page Content -->
+<div class="container">
+
+    <section id="registration">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-xs-offset-3">
+                    <div class="form-wrap">
+                        <h1 class="text-center">Register</h1>
+                        <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
+                            <div class="form-group">
+                                <label for="username" class="sr-only">username</label>
+                                <input type="text" name="username" id="username" class="form-control"
+                                       placeholder="Enter Desired Username">
+                            </div>
+                            <div class="form-group">
+                                <label for="email" class="sr-only">Email</label>
+                                <input type="email" name="email" id="email" class="form-control"
+                                       placeholder="somebody@example.com">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="sr-only">Password</label>
+                                <input type="password" name="password" id="key" class="form-control"
+                                       placeholder="Password">
+                            </div>
+
+                            <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block"
+                                   value="Register">
+                        </form>
+
+                    </div>
+                </div> <!-- /.col-xs-12 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </section>
+
+    <hr>
+
+    <!-- Footer -->
+    <?php include "includes/footer.php"; ?>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>

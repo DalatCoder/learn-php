de319e4972f16e5a785980fba1a8931c7ac4f2d8
Trong Hieu <hieuntctk42@gmail.com>
2021-01-15 10:26:45 +0700

    Display comment image

--- cms-blog/post.php
+++ cms-blog/post.php
@@ -139,7 +139,7 @@
                         ?>
                         <div class="media">
                             <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
+                                <img class="media-object" src="admin/images/default.png" alt="">
                             </a>
                             <div class="media-body">
                                 <h4 class="media-heading"><?php echo $comment_author; ?>

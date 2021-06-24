c6bc966704a4505c5927edd4838c9a75894b29df
DalatCoder <hieuntctk42@gmail.com>
2021-06-24 21:08:28 +0700

    Update 🐇

--- pluralsight/03.authentication-and-authorization/form-based/index.php
+++ pluralsight/03.authentication-and-authorization/form-based/index.php
@@ -23,11 +23,11 @@ if (!Authenticate::isLoggedIn()) {
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <title>Hello, world!</title>
+  <title>Home Page</title>
 </head>
 
 <body>
-  <h1>Hello User</h1>
+  <h1>Hello User: <?php echo $_SESSION['identity'] ?></h1>
 
   <!-- Optional JavaScript; choose one of the two! -->
 
